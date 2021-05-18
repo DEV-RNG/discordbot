b1d6a1963973f6330e7bf2e8020be68761da3251
DEV-RNG <paskiki1423@outlook.com>
2021-05-18 16:01:37 -0400

    Create index.js

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
 const Discord = require('discord.js');
 const bot = new Discord.Client({DisableEveryone: true});
+
+bot.on('ready', async () =>{
+  bot.user.setActivity('bot tutoriel. Bonjours!');
+});
+
+bot.login(process.env.BOT_TOKEN);
