2348185173fc0f8efb145ea0f1ab0eeaafe7eaa9
DEV-RNG <paskiki1423@outlook.com>
2021-05-18 19:22:18 -0400

    Update index.js

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ bot.on('ready', async () =>{
 
 bot.on('message', async (msg) => {
     if(msg.content.startsWith(config.prefix) && !msg.author.bot){
-        cmdArray = msg.content.substring(1).split(" ")
+        cmdArray = msg.content.substring(config.prefix.length).split(" ")
         cmd = cmdArray[0]
         args = cmdArray.slice(1)
 
@@ -20,7 +20,7 @@ bot.on('message', async (msg) => {
           if(args.length < 1) return msg.channel.send("Pas assez d'arguments.")
           var message = ""
           for(let i = 0; i < args.length; i++){
-              message += args[i] + ""
+              message += args[i] + " "
           }
           msg.channel.send("message" + message)
         }
