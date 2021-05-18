49e5582650acceeb8226e7ee57943243645d4dc2
DEV-RNG <paskiki1423@outlook.com>
2021-05-18 16:24:31 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
 const Discord = require('discord.js');
 const bot = new Discord.Client({DisableEveryone: true});
+const config = require('./config.js');
 
 bot.on('ready', async () =>{
   console.log('Le bot est lancer');
   bot.user.setActivity('bot tutoriel. Bonjours!');
+});
+
+bot.on('message', async (msg) => {
+    if(msg.content == 'bonjour'){
+        msg.channel.send('Salut!')
+    }
 });
 
 bot.login(process.env.BOT_TOKEN);
