95e431f44e66c200e546b0129eca8643e02b7380
DEV-RNG <paskiki1423@outlook.com>
2021-05-18 17:52:02 -0400

    Update commands.js

--- commands.js
+++ commands.js
@@ -12,7 +12,7 @@ const commandFactory = (command) => {
 };
 
 const getCommand = (name) => {
-    return commands.find((command) => command.name === name ));
+    return commands.find((command) => command.name === name );
 };
 
 const getCommandList = () => {
@@ -27,7 +27,7 @@ fs.readdir('./commands/', (err, files) => {
     commandFiles.forEach((commandFile) => {
       const command = require(`./commands/${commandFile}`);
 
-      commands.push(command);
+      commands.push(commandFactory);
     });
 });
 
