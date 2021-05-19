ebc275f26e79e7c834b481c8bb823aa082dc475a
DEV-RNG <paskiki1423@outlook.com>
2021-05-19 12:27:07 -0400

    Update ip.js

--- commands/ip.js
+++ commands/ip.js
@@ -3,7 +3,7 @@ const Discord = require('discord.js');
 module.exports.run = async (bot, msg, args) => {
     let hEmbed = new Discord.MessageEmbed()
     .setColor('0047FF')
-    .setTitle('RealLife Bot')
+    .setTitle('RealLife Bot (F8)')
     .addField('Direct Connect',
     '- connect 51.161.116.127:30112')
     msg.channel.send(hEmbed);
